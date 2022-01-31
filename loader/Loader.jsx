6a1eed54f92d80089cf7e719dcc6f1bfe33203ac
facobackup <gustavomicaelbarbosa@gmail.com>
2022-01-30 21:34:55 -0300

    Ajustes

--- loader/Loader.jsx
+++ loader/Loader.jsx
@@ -5,20 +5,22 @@ import PropTypes from "prop-types";
 
 export default function Loader(props){
     return(
-        <div
-            className={styles.loader}
-            style={{background: props.dark ? '#303030' : 'white', color: props.dark ? 'white' : '#333333'}}
-        >
-            {Object.keys(props.events).map((e, i) => (
-                <div className={styles.loadInfo} key={e+'-loading-indicator-'+i}>
-                    {e}
-                    <div data-index={`${i}`} className={styles.loaderBar} style={{background: props.dark ? '#111111' : ' #ced4da',}}>
-                        <div className={styles.loading} style={{
-                            animationDelay: `${i * 100}ms`,
-                            '--animationSpeed': randomInRange(2, 1) + 's'}}/>
+        <div className={styles.wrapper}>
+            <div
+                className={styles.loader}
+                style={{background: props.dark ? '#303030' : 'white', color: props.dark ? 'white' : '#333333'}}
+            >
+                {Object.keys(props.events).map((e, i) => (
+                    <div className={styles.loadInfo} key={e+'-loading-indicator-'+i}>
+                        {e}
+                        <div data-index={`${i}`} className={styles.loaderBar} style={{background: props.dark ? '#111111' : ' #ced4da',}}>
+                            <div className={styles.loading} style={{
+                                animationDelay: `${i * 100}ms`,
+                                '--animationSpeed': randomInRange(2, 1) + 's'}}/>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
