49080ef4e94e550058ac5c2ab42f885742d8822b
facobackup <gustavomicaelbarbosa@gmail.com>
2022-06-14 11:29:57 -0300

    [OPTIMIZATION] - [useDirectState push block method; Less unnecessary re-renders]

--- hooks/useDirectState.js
+++ hooks/useDirectState.js
@@ -2,10 +2,11 @@ import {useMemo, useState} from "react"
 
 export default function useDirectState(initialState={}){
     const [state, setState] = useState(initialState)
-    return [useMemo(() => {
-        return new Proxy(state, {
+    return [
+        useMemo(() => {
+            return new Proxy(state, {
                 get(obj, key) {
-                    return obj[key];
+                    return obj[key]
                 },
                 set(obj, key, value) {
                     setState(prev => {
@@ -17,6 +18,15 @@ export default function useDirectState(initialState={}){
                     return true
                 }
             }
-        )
-    }, [state]), () => setState({})]
+            )
+        }, [state]), 
+        () => setState({}),
+        (data) => {
+            const newState = {...state}
+            Object.keys(data).forEach(d => {
+                if(data[d])
+                    newState[d] = data[d]
+            })
+            setState(newState)
+        }]
 }
