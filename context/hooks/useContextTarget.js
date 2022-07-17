b2ae2581eda7834db600f74496a0dd421eef9a5f
facobackup <gustavomicaelbarbosa@gmail.com>
2022-07-17 18:02:44 -0300

    [MIX] - [Reworking file deletion on content browser; code refactoring]

--- context/hooks/useContextTarget.js
+++ context/hooks/useContextTarget.js
@@ -2,7 +2,6 @@ import {useEffect} from "react"
 
 export default function useContextTarget(targetRef, options, triggers) {
     useEffect(() => {
-        console.log(targetRef)
         window.contextMenu.targets[typeof targetRef === "string" ? targetRef : targetRef.id] = {
             options: options,
             triggers: triggers
